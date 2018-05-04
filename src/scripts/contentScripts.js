da4a796c97a32aa6e35c97415e29cb8909e99e8b
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-05-05 01:12:18 +0900

    implement murlizer

--- src/scripts/contentScripts.js
+++ src/scripts/contentScripts.js
@@ -1,1 +1,27 @@
-console.log("test");
+import domready from "domready";
+import Murlifier from "murlifier";
+import path from "path";
+
+const dictUrl = chrome.extension.getURL("dict/");
+const murlifier = new Murlifier(dictUrl);
+
+domready(async () => {
+  new Murlizer(murlifier, document.querySelectorAll("p")).apply();
+});
+
+class Murlizer {
+  constructor(murlifier, doms) {
+    this.murlifier = murlifier;
+    this.doms = Array.from(doms);
+  }
+
+  apply() {
+    for (let dom of this.doms) {
+      this.murlize(dom);
+    }
+  }
+
+  async murlize(dom) {
+    dom.innerHTML = await this.murlifier.murlify(dom.innerHTML);
+  }
+}
